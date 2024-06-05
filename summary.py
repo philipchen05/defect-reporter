3b25218278602cc19378ac04f8e3435eaa9f3620
Philip <philipchen935@gmail.com>
2024-06-05 16:01:43 -0400

    Removed unnecessary import

--- summary.py
+++ summary.py
@@ -6,7 +6,6 @@ import openpyxl
 from openpyxl import load_workbook
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 import shutil
 import time
